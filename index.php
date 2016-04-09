c07395f86cb17754cb3e7c009c45aa3f891cd54a
KeisukeMatsukawa <matsukawa@soleil-d.com>
2016-04-10 03:25:17 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>sample</title>
+<meta name="keywords" content="">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<link href="https://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="css/common.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<h1>まずは入力のページです</h1>
+
+<form action="create.php" method="post">
+	
+	<input type="text" name="memo" />
+	
+	<input type="submit" />
+	
+</form>
+
+</body>
+</html>
